cfced2fc17ddc0e33604c05e0bbf32508fee863b
alisasu <alisa.su97@gmail.com>
2019-10-09 00:32:34 -0400

    new commit
    check mobile

---
+++ js/mobile-check.js
@@ -0,0 +1,19 @@
+function detectmob() { 
+ if( navigator.userAgent.match(/Android/i)
+ || navigator.userAgent.match(/webOS/i)
+ || navigator.userAgent.match(/iPhone/i)
+ || navigator.userAgent.match(/iPad/i)
+ || navigator.userAgent.match(/iPod/i)
+ || navigator.userAgent.match(/BlackBerry/i)
+ || navigator.userAgent.match(/Windows Phone/i)
+ ){
+    return true;
+  }
+ else {
+    return false;
+  }
+}
+
+if detectmob() {
+	document.write("Looks like you're on mobile! Unfortunately, I haven't made this website to be mobile-friendly. View on desktop for best results!")
+}
