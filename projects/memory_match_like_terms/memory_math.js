34295147a03581139e84c4380424fd6797893761
alisasu <alisa.su97@gmail.com>
2021-01-05 20:12:06 -0500

    added sum feature to memory matching game

--- projects/memory_match_like_terms/memory_math.js
+++ projects/memory_match_like_terms/memory_math.js
@@ -51,40 +51,70 @@ function handler(e){
 function flip(e) {
     if (first != e.target) {
         
-    console.log(e.target.innerHTML);
-    e.target.innerHTML = terms[parseInt(e.target.id)];
-    e.target.style.background = "cornflowerblue";
-    if (!(findingSecond)) {
-        first = e.target;
-        console.log(first);
-        findingSecond = true;
-    }
-    else {
-        if (terms[parseInt(e.target.id)].split(" ")[1] === terms[parseInt(first.id)].split(" ")[1] ) {
-            console.log("match!");
-            e.target.style.background = "lightgreen";
-            first.style.background = "lightgreen";
-            /*
-            document.addEventListener('click', handler, true);
-            document.getElementById('popup').style.visibility = "visible";
-            document.getElementById('expression').innerHTML = `${terms[parseInt(e.target.id)]} + ${terms[parseInt(first.id)]} = ? `;
-            */
-        } 
-        else {
-            document.addEventListener('click', handler, true);
-            setTimeout(function() {
-                e.target.innerHTML = "";
-                first.innerHTML = "";
-                e.target.style.background = "cadetblue";
-                first.style.background = "cadetblue";
-                document.removeEventListener('click', handler, true);
-            }, 1500);
-            
+        console.log(e.target.innerHTML);
+        e.target.innerHTML = terms[parseInt(e.target.id)];
+        e.target.style.background = "cornflowerblue";
+        if (!(findingSecond)) {
+            first = e.target;
+            console.log(first);
+            findingSecond = true;
         }
-        findingSecond = false;
+        else {
+            if (terms[parseInt(e.target.id)].split(" ")[1] === terms[parseInt(first.id)].split(" ")[1] ) {
+                console.log("match!");
+                e.target.style.background = "lightgreen";
+                first.style.background = "lightgreen";
+                
+                //document.addEventListener('click', handler, true);
+
+                document.getElementById('popup').classList.toggle("show");   
+                //couldn't find a way to disable clicks outside modal so i removed all eventlisteners instead
+                [].forEach.call( document.getElementsByClassName('card'), item => item.removeEventListener("click", flip));
+                document.getElementById('expression').innerHTML = `${terms[parseInt(e.target.id)]} + ${terms[parseInt(first.id)]} = ? `;
+                document.querySelector("#submit_sum").addEventListener("click", function() {
+                    console.log("running the if statement");
+                    if ( `${parseInt(terms[parseInt(e.target.id)].split(" ")[0]) + parseInt(terms[parseInt(first.id)].split(" ")[0])} ${terms[parseInt(e.target.id)].split(" ")[1]}`.replace(" ", "").replace("<sup>", "^").replace("</sup>", "") === document.getElementById("sum").value ) {
+                        document.getElementById("correct").innerHTML = "Yes!";
+                        setTimeout( function() {
+                        e.target.style.visibility = "hidden";
+                        first.style.visibility = "hidden";
+                        document.getElementById("correct").innerHTML = "";
+                        document.getElementById('popup').classList.remove("show");
+                        first = "";
+                        [].forEach.call( document.getElementsByClassName('card'), item => item.addEventListener("click", flip));
+                        }, 3000);
+                    }
+                    else {
+                        document.getElementById("correct").innerHTML = "Nope, but you can try again in a few seconds!";
+                        setTimeout( function() {
+                            e.target.innerHTML = "";
+                            first.innerHTML = "";
+                            e.target.style.background = "cadetblue";
+                            first.style.background = "cadetblue";
+                            document.getElementById("correct").innerHTML = "";
+                            document.getElementById('popup').classList.remove("show");
+                            [].forEach.call( document.getElementsByClassName('card'), item => item.addEventListener("click", flip));
+                        }, 3000);
+                    }
+
+                });
+
+            } 
+            else {
+                document.addEventListener('click', handler, true);
+                setTimeout(function() {
+                    e.target.innerHTML = "";
+                    first.innerHTML = "";
+                    e.target.style.background = "cadetblue";
+                    first.style.background = "cadetblue";
+                    document.removeEventListener('click', handler, true);
+                }, 1500);
+                
+            }
+            findingSecond = false;
+        }
     }
 }
-}
+
 
 [].forEach.call( document.getElementsByClassName('card'), item => item.addEventListener("click", flip));
-
